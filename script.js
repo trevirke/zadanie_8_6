0d0d8a48840ebe785c8a7a379c24d13eb5470d7f
Sylwia Mamczur <sylwia.mamczur@gmail.com>
2017-11-24 13:14:22 +0100

    Add files

---
+++ script.js
@@ -0,0 +1,15 @@
+var a = 2;
+var b = 5;
+
+var value = (a * a) - (2 * a * b) + (b * b);
+console.log(value);
+
+if (value > 0) {
+  console.log('wynik dodatni');
+} 
+else if (value = 0) {
+  console.log('wynik to 0');
+} 
+else {
+  console.log('wynik ujemny');
+}
